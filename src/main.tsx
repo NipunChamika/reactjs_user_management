90cb5be4042327a517b6e815798bf154c38ba9e4
Nipun Chamika <nipunchamika11@gmail.com>
2023-12-08 19:29:13 +0530

    Import PrimeFlex

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import "./index.css";
 import "primereact/resources/themes/saga-blue/theme.css"; // theme
 import "primereact/resources/primereact.min.css"; // core css
 import "primeicons/primeicons.css"; // icons
+import "primeflex/primeflex.css"; // flex
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>

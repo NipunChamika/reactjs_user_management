fba98c0eb1172ee35e1019737ea40eeb8b8dc4cf
Nipun Chamika <nipunchamika11@gmail.com>
2023-12-08 19:09:09 +0530

    Configure global fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "bootstrap/dist/css/bootstrap.css";
+import "./index.css";
+
+// PrimeReact CSS
+import "primereact/resources/themes/saga-blue/theme.css"; // theme
+import "primereact/resources/primereact.min.css"; // core css
+import "primeicons/primeicons.css"; // icons
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
